8cff6366c89e5da7076dcb5dbb9bff43faaab559
DimitarPelov <dimi.pelov@gmail.com>
2025-03-19 13:55:37 +0200

    updated README.md

--- src/main/java/org/example/Request.java
+++ src/main/java/org/example/Request.java
@@ -1,16 +1,49 @@
 package org.example;
 import packageEnum.Status;
 
+import java.time.LocalDateTime;
+
 public class Request {
-//
+    private Integer requestId;
     private Client client;
     private Status status;
+    private Car car;
+    private LocalDateTime createdOn;
+    private LocalDateTime completedOn;
     private boolean modifiedRequest;
 
-    public Request(Client client, Status status, boolean modifiedRequest) {
+    public Request(){
+        this.createdOn= LocalDateTime.now();
+    }
+    public Request(Integer requestId, Client client, Status status, Car car, LocalDateTime createdOn, LocalDateTime completedOn, boolean modifiedRequest) {
+        this.requestId = requestId;
         this.client = client;
         this.status = status;
+        this.car = car;
+        this.createdOn = createdOn;
+        this.completedOn = completedOn;
         this.modifiedRequest = modifiedRequest;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "requestId=" + requestId +
+                ", client=" + client +
+                ", status=" + status +
+                ", car=" + car +
+                ", createdOn=" + createdOn +
+                ", completedOn=" + completedOn +
+                ", modifiedRequest=" + modifiedRequest +
+                '}';
+    }
+
+    public Integer getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(Integer requestId) {
+        this.requestId = requestId;
     }
 
     public Client getClient() {
@@ -29,6 +62,30 @@ public class Request {
         this.status = status;
     }
 
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public LocalDateTime getCompletedOn() {
+        return completedOn;
+    }
+
+    public void setCompletedOn(LocalDateTime completedOn) {
+        this.completedOn = completedOn;
+    }
+
     public boolean isModifiedRequest() {
         return modifiedRequest;
     }
@@ -36,6 +93,4 @@ public class Request {
     public void setModifiedRequest(boolean modifiedRequest) {
         this.modifiedRequest = modifiedRequest;
     }
-
-
 }
